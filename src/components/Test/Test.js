0c885113689f27ba148eea9204e53a812040d137
Mihai Dornescu <dornescu@gmail.com>
2021-05-01 22:36:19 +0300

    test form push data

--- src/components/Test/Test.js
+++ src/components/Test/Test.js
@@ -8,45 +8,19 @@ const stopDefault = (e) => {
 }
 
 const Test = () => {
-    const lucrari = [
-        {job: 'cadastru și intabulare apartament', id: 1},
-        {job: 'cadastru și intabulare teren extravilan', id: 2},
-        {job: 'actualizare / rectificare cadastru', id: 3},
-        {job: 'dezmembrare / parcelare / lotizare teren extravilan', id: 4},
-        {job: 'alipire teren extravilan', id: 5},
-        {job: 'plan topografic (de situație)', id: 6},
-        {job: 'scoaterea din circuitul agricol', id: 7},
-        {job: 'alt tip de lucrare', id: 8},
-        {job: 'cadastru și intabulare teren intravilan', id: 9},
-        {job: 'cadastru și intabulare teren cu construcție', id: 10},
-        {job: 'dezmembrare / parcelare / lotizare teren intravilan', id: 11},
-        {job: 'alipire teren intravilan', id: 12},
-        {job: 'alipire unitati individuale (apartamente)', id: 13},
-        {job: 'trasare / înțărușare teren / construcție', id: 14},
-        {job: 'aviz tehnic OCPI', id: 15},
 
-    ]
-    const changeColor =()=>{
-        // const color = {backgroundColor: 'red', fontSize: '44'}
-        // return color
-        console.log('click')
-    }
 
     return <ContentWrapper>
+
+        <div className="content-heading">
+            <div>Date fiscale
+                <p className='font-para-title'>Actualizati datele fiscale ale firmei / PFA. Sunt necesare in
+                    procesul de facturare.</p>
+            </div>
+        </div>
         <Card>
             <CardBody>
-                <FormGroup row className='formular-lucrari'>
-                    {lucrari.map((item, id) => {
-                        return <Test1 job={item.job} key={item.id} click={changeColor}/>
-                    })}
-                    <div className="col-12 btn-lucrari">
-                        <div className="ml-auto">
-                            <button type="submit" className="btn btn-primary">
-                                <span className="btn-primary"><i className="fa fa-save"> </i></span> Salvare
-                            </button>
-                        </div>
-                    </div>
-                </FormGroup>
+                <Test1 />
             </CardBody>
         </Card>
 
@@ -57,3 +31,39 @@ const Test = () => {
 
 export default Test;
 
+// const lucrari = [
+//     {job: 'cadastru și intabulare apartament', id: 1},
+//     {job: 'cadastru și intabulare teren extravilan', id: 2},
+//     {job: 'actualizare / rectificare cadastru', id: 3},
+//     {job: 'dezmembrare / parcelare / lotizare teren extravilan', id: 4},
+//     {job: 'alipire teren extravilan', id: 5},
+//     {job: 'plan topografic (de situație)', id: 6},
+//     {job: 'scoaterea din circuitul agricol', id: 7},
+//     {job: 'alt tip de lucrare', id: 8},
+//     {job: 'cadastru și intabulare teren intravilan', id: 9},
+//     {job: 'cadastru și intabulare teren cu construcție', id: 10},
+//     {job: 'dezmembrare / parcelare / lotizare teren intravilan', id: 11},
+//     {job: 'alipire teren intravilan', id: 12},
+//     {job: 'alipire unitati individuale (apartamente)', id: 13},
+//     {job: 'trasare / înțărușare teren / construcție', id: 14},
+//     {job: 'aviz tehnic OCPI', id: 15},
+//
+// ]
+// const changeColor =()=>{
+//     // const color = {backgroundColor: 'red', fontSize: '44'}
+//     // return color
+//     console.log('click')
+// }
+
+{/*<FormGroup row className='formular-lucrari'>*/}
+{/*    {lucrari.map((item, id) => {*/}
+{/*        return <Test1 job={item.job} key={item.id} click={changeColor}/>*/}
+{/*    })}*/}
+{/*    <div className="col-12 btn-lucrari">*/}
+{/*        <div className="ml-auto">*/}
+{/*            <button type="submit" className="btn btn-primary">*/}
+{/*                <span className="btn-primary"><i className="fa fa-save"> </i></span> Salvare*/}
+{/*            </button>*/}
+{/*        </div>*/}
+{/*    </div>*/}
+{/*</FormGroup>*/}
--- src/components/Test/Test1.js
+++ src/components/Test/Test1.js
@@ -1,21 +1,166 @@
-import React from "react";
-import {Input} from "reactstrap";
+import React, {useState} from "react";
+import {CardFooter, Col, FormGroup, Input, Row} from "reactstrap";
 import '../TipuriLucrari/tipuri-lucrari.scss'
 
-const Test1 =(props)=>{
-    const style ={
-        backgroundColor: "$primary",
-        padding: ".5rem",
+const Test1 = (props) => {
+    const [enteredName, setEnteredName] = useState('');
+    const [enteredCUI, setEnteredCUI] = useState('');
+    const [enteredNr, setEnteredNr] = useState('');
+    const [enteredCounty, setEnteredCounty] = useState('');
+    const [enteredTown, setEnteredTown] = useState('');
+    const [enteredAdress, setEnteredAdress] = useState('');
+
+    const nameHandler = (e) =>{
+        setEnteredName(e.target.value);
     }
-const change =()=>{
-    // console.log('clicked')
+      const CUIHandler = (e) =>{
+        setEnteredCUI(e.target.value);
+    }
+      const nrHandler = (e) =>{
+        setEnteredNr(e.target.value);
+    }
+      const countyHandler = (e) =>{
+        setEnteredCounty(e.target.value);
+    }
+      const townHandler = (e) =>{
+        setEnteredTown(e.target.value);
+    }
+      const adressHandler = (e) =>{
+        setEnteredAdress(e.target.value);
+    }
+
+
+    const values =[]
+
+    const inputValue = e => {
+        e.preventDefault();
+        const data = {
+            name: enteredName,
+            cui: enteredCUI,
+            nr: enteredNr,
+            county: enteredCounty,
+            town: enteredTown,
+            adress: enteredAdress
+        }
+
+        setEnteredName('');
+        setEnteredCUI('');
+        setEnteredNr('');
+        setEnteredCounty('');
+        setEnteredTown('');
+        setEnteredAdress('');
+        console.log(data)
+    }
+
+
+
+    return <form action="" onSubmit={inputValue} >
+        <Row>
+            <Col lg='6'>
+                <FormGroup>
+                    <label>Nume *</label>
+                    <Input type="text" placeholder="nume" value={enteredName} onChange={nameHandler}
+                    />
+                </FormGroup>
+
+                <FormGroup>
+                    <label>CUI *</label>
+                    <Input type="text" placeholder="cui" value={enteredCUI}  onChange={CUIHandler}/>
+                </FormGroup>
+                <FormGroup>
+                    <label>Nr Registru Comert *</label>
+                    <Input type="number" placeholder="nr" value={enteredNr} onChange={nrHandler} />
+                </FormGroup>
+
+                <fieldset style={{padding: "0", marginBottom: "10"}}>
+                    <FormGroup row>
+                        <label className="col-md-12 col-form-label">Judet *</label>
+                        <div className="col-md-12">
+                            <select defaultValue="" className="custom-select custom-select-sm" onChange={countyHandler}
+
+                            >
+                                <option  value={enteredCounty}>Alege judet</option>
+                                <option defaultValue="1">Alba</option>
+                                <option defaultValue="2">Arad</option>
+                                <option defaultValue="3">Arges</option>
+                            </select>
+                        </div>
+                    </FormGroup>
+                </fieldset>
+                {/*================*/}
+                <FormGroup>
+                    <label>Localitate *</label>
+                    <Input type="text" placeholder="localitate" value={enteredTown} onChange={townHandler} />
+                </FormGroup>
+
+                {/*==============*/}
+                <div className="form-group row">
+                    <label className="col-md-3 col-4 col-form-label " htmlFor="inputContact6">Adresa
+                        sediu social *</label>
+                    <div className="col-xl-12 col-md-12 col-12">
+                                            <textarea className="form-control" id="inputContact6" row="3"
+                                                      placeholder="Some nice Street, 1234" onChange={adressHandler}
+                                            value={enteredAdress}
+                                            ></textarea>
+                    </div>
+                </div>
+            </Col>
+            <Col lg='6'>
+                <FormGroup>
+                    <label>Telefon</label>
+                    <Input type="number" placeholder="telefon"/>
+                </FormGroup>
+                <FormGroup>
+                    <label>Fax</label>
+                    <Input type="number" placeholder="fax"/>
+                </FormGroup>
+                <FormGroup>
+                    <label>Email</label>
+                    <Input type="email" placeholder="@email"/>
+                </FormGroup>
+                <FormGroup>
+                    <label>Banca</label>
+                    <Input type="text" placeholder="banca"/>
+                </FormGroup>
+                <FormGroup>
+                    <label>IBAN</label>
+                    <Input type="text" placeholder="iban"/>
+                </FormGroup>
+
+
+
+                {/*<button className="btn btn-sm btn-secondary" type="submit">Submit</button>*/}
+                <CardFooter>
+                    <div className="checkbox c-checkbox" style={{padding: "1rem"}}>
+                        <label>
+                            <Input type="checkbox" defaultChecked=""/>
+                            <span className="fa fa-times"></span>Check me out</label>
+                    </div>
+                    <button className="btn btn-sm btn-secondary" type="submit">Submit</button>
+                </CardFooter>
+            </Col>
+        </Row>
+
+
+    </form>
+
+
 }
-    return <div className="col-md-6" style={style} key={props.id}>
-        <div className="checkbox c-checkbox">
-            <label className='lucrare' onClick={props.click}>
-                <Input type="checkbox" defaultChecked/>
-                <span className="fa fa-check"></span>{props.job}</label>
-        </div>
-    </div>
-}
+
+
 export default Test1;
+
+// const style ={
+//     backgroundColor: "$primary",
+//     padding: ".5rem",
+// }
+// const change =()=>{
+//     // console.log('clicked')
+// }
+// return <div className="col-md-6" style={style} key={props.id}>
+//     <div className="checkbox c-checkbox">
+//         <label className='lucrare' onClick={props.click}>
+//             <Input type="checkbox" defaultChecked/>
+//             <span className="fa fa-check"></span>{props.job}</label>
+//     </div>
+// </div>
