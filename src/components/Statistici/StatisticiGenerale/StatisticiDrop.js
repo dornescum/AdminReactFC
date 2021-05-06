0a19c169572ebefd932d780b2f686d4405dc0386
Mihai Dornescu <dornescu@gmail.com>
2021-05-06 20:55:31 +0300

    statistici generale neterminat

--- src/components/Statistici/StatisticiGenerale/StatisticiDrop.js
+++ src/components/Statistici/StatisticiGenerale/StatisticiDrop.js
@@ -1,8 +1,33 @@
-const StatisticiDrop =()=>{
+import React from "react";
+import {Col, Row} from "reactstrap";
+
+const StatisticiDrop = () => {
     return <div className='statistici-perioada'>
-        <p>dsdad</p>
-        <p>dsdad</p>
-        <p>dsdad</p>
+        <Row>
+            <Col lg='4'>
+                <h6 className='cauta-perioada'>Selectati perioada</h6>
+            </Col>
+            <Col lg='4'>
+
+                <select defaultValue="" className="custom-select" multiple="">
+                    <option>Luna</option>
+                    <option defaultValue="1">Ianuarie</option>
+                    <option defaultValue="2">Februarie</option>
+                    <option defaultValue="3">Martie</option>
+                </select>
+            </Col>
+            <Col lg='4'>
+                <select defaultValue="" className="custom-select" multiple="">
+                    <option>Anul</option>
+                    <option defaultValue="1">2001</option>
+                    <option defaultValue="2">2002</option>
+                    <option defaultValue="3">2003</option>
+                </select>
+            </Col>
+
+        </Row>
+
+
     </div>
 }
 export default StatisticiDrop;
