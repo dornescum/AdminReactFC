bf7bcc47b5bbbf1bb37c48e11dfae006eeb3028b
Mihai Dornescu <dornescu@gmail.com>
2021-10-07 20:28:39 +0300

    trim extra

--- src/App.js
+++ src/App.js
@@ -1,14 +1,3 @@
-/*!
- *
- * Angle - Bootstrap Admin Template
- *
- * Version: 4.8
- * Author: @themicon_co
- * Website: http://themicon.co
- * License: https://wrapbootstrap.com/help/licenses
- *
- */
-
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -25,10 +14,6 @@ import './styles/app.scss'
 class App extends Component {
   render() {
 
-    // specify base href from env varible 'PUBLIC_URL'
-    // use only if application isn't served from the root
-    // for development it is forced to root only
-    /* global PUBLIC_URL */
     const basename = process.env.NODE_ENV === 'development' ? '/' : (PUBLIC_URL || '/');
 
     return (
